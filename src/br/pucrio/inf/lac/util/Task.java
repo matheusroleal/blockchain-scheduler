013ec43374bacb444ba85fdb423238fd4a422040
matheus.leal <matheus.leal@corp.globo.com>
2019-11-16 12:31:30 -0300

    Task update

--- src/br/pucrio/inf/lac/util/Task.java
+++ src/br/pucrio/inf/lac/util/Task.java
@@ -23,17 +23,26 @@ public class Task extends TimerTask{
   }
   
   public void add(Transaction t) {
+	  // INIT -- Período de testes
+	  System.out.println(t.getData());
+	  // END -- Período de testes
 	  this.transaction.add(t);
   }
 
   @Override
   public void run() {
+	System.out.println(this.transaction.size());
 	if( this.transaction.size() > 0) {
 		// INIT -- Período de testes		
 		System.out.println("Novos dados");
 		// END -- Período de testes
 		
-		List<Transaction> transactions_to_send = this.transaction;
+		List<Transaction> transactions_to_send = new ArrayList<>();
+		
+		for(Transaction t : transaction){
+			transactions_to_send.add(t);
+		}
+		
 		this.transaction.clear();
 
 		// INIT -- Período de testes		
@@ -44,6 +53,11 @@ public class Task extends TimerTask{
 		Current.getCurrent(BlockchainType).Send(transactions_to_send);
 	    
 	    transactions_to_send.clear();
+
+		// INIT -- Período de testes		
+		System.out.println(this.transaction.size());
+		System.out.println(transactions_to_send.size());
+		// END -- Período de testes		
 	}else {
 		System.out.println("Sem Novos dados");
 	}
